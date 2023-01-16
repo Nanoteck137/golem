d3180a00b9d1277bfe7c435fcd6b0e94cf61ea53
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-01-16 02:04:53 +0100

    Reading config file

--- gabite/src/main.rs
+++ gabite/src/main.rs
@@ -1,28 +1,63 @@
-use std::{time::Duration, sync::RwLock};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::time::Duration;
 
-use rocket::tokio::{spawn, time};
+use rocket::tokio::{self, time};
+use serde::Deserialize;
 
-static NUMBER: RwLock<u32> = RwLock::new(0);
+#[derive(Deserialize, Debug)]
+struct Config {
+    machines: Vec<Machine>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Machine {
+    name: String,
+    ip: String,
+    api_port: u16,
+}
+
+struct MachineData {}
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
 fn test() -> String {
-    let number = NUMBER.read().unwrap();
-    format!("Number: {}", number)
+    format!("Test")
+}
+
+fn fetch() {
+    // println!("Fetch Data from machines");
+}
+
+fn read_file<P>(filepath: P) -> String
+where
+    P: AsRef<Path>,
+{
+    let mut file =
+        File::open(filepath).expect("Failed to open file for reading");
+    let mut result = String::new();
+    file.read_to_string(&mut result)
+        .expect("Failed to read file");
+
+    result
 }
 
 #[launch]
 fn rocket() -> _ {
-    spawn(async move {
+    let config = read_file("config.json");
+    let config = serde_json::from_str::<Config>(&config)
+        .expect("Failed to parse config.json");
+
+    println!("Config: {:#?}", config);
+
+    tokio::spawn(async move {
         let mut interval = time::interval(Duration::from_secs(1));
         loop {
             interval.tick().await;
-            {
-                let mut lock = NUMBER.write().unwrap();
-                *lock = *lock + 1;
-            }
+            fetch();
         }
     });
 
